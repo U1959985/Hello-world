2caac87ed806f0fb98afce358068fd4b6645a0b7
U1959985 <U1959985@unimail.hud.ac.uk>
2021-01-21 14:54:29 +0000

    DEMO

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * A demo class.
+ *
+ * @author Kyle Rowlands
+ * @version January 2021
+ */
+public class HelloWorld {
+    /**
+     * A demo method.
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
